a1711459d81efeee0f9866b3f88956bf7e470eb6
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2022-11-10 16:03:42 -0300

    Correções de tempo

--- MDC Recursivo.c
+++ MDC Recursivo.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-int maximodiv(int a, int b, int r);
+int maximodiv(int a, int b);
 
 int main()
 {
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("MDC = %d", maximodiv(a, b, b));
+    printf("MDC = %d", maximodiv(a, b));
 }
 
-int maximodiv(int a, int b, int r)
+int maximodiv(int a, int b)
 {
-    if (r == 0)
-        return a;
-
-    r = (a % b);
+    int r = (a % b);
     printf("%d,%d\n", a, b);
 
     /*a=b;
     b=r;*/
-    maximodiv(b, r, r);
+    if (r == 0)
+        return b;
+    maximodiv(b, r);
 }
+/*
+* Equação de recorrência: 
+* {O(1) se aMODb=0
+* {t(b, a%b)+f(a) caso contrario
+*/
